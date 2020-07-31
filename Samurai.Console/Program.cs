112dbf1fe1f7dea4e77727e17d39de8f9bd7a6f1
Richard Orchard <me@richardorchard.com>
2020-07-31 10:58:47 +0100

    initial EF migrations

---
+++ Samurai.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System.Linq;
+
+namespace Samurai.Console
+{
+    class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+
+
+        static void Main(string[] args)
+        {
+
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            System.Console.Write("Press any key...");
+            System.Console.ReadKey();
+
+        }
+
+        private static void AddSamurai()
+        {
+
+            var samurai = new SamuraiApp.Domain.Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            System.Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                System.Console.WriteLine(samurai.Name);
+            }
+        }
+
+
+
+    }
+}
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
 using System;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext: DbContext
     {
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        public DbSet<Clan> Clans { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+
+
+        }
 
 
     }
